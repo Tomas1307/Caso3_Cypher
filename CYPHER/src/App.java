39e6589ecf02fef8d9eea9167cfc041e41591934
Tomas1307 <t.acosta@uniandes.edu.co>
2022-11-03 10:48:52 -0500

    Aplicacion y Primera parte cifrado

--- CYPHER/src/App.java
+++ CYPHER/src/App.java
@@ -1,6 +1,49 @@
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class App {
 	public static void main(String[] args) throws Exception {
-	
-	
+		
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Ingrese 'SECURE INIT' si desea comenzar con el programa ");
+		
+		String iniciar = sc.next();
+		
+		Boolean centinela = true; 
+		
+	    BigInteger Pmain = null;
+		
+		BigInteger Gmain = null;
+		
+		BigInteger Gxmain = null;
+		
+		if( iniciar.equals("SECURE INIT")) {
+		System.out.println("Para detener la ejecucion precione la tecla '.' ");
+		
+		while (centinela) {
+		
+			System.out.println("Ingrese un valor primo P: ");
+			Pmain = sc.nextBigInteger();
+			
+			System.out.println("Ingrese un valor para G (<P) : ");
+			Gmain = sc.nextBigInteger();
+			
+			
+			System.out.println("Ingrese un valor para Gx (<p-1): ");
+			Gxmain = sc.nextBigInteger();
+			centinela = false;
+			
+			}
+		
+		Cifrado cypher = new Cifrado(Gmain,Pmain,Gxmain);
+			
+			
+		}
+	}
 }
+
+	
+	
+
--- CYPHER/src/Cifrado.java
+++ CYPHER/src/Cifrado.java
@@ -1,4 +1,24 @@
+import java.math.BigInteger;
+import java.security.PublicKey;
 
 public class Cifrado {
+	
+	private BigInteger y ;
+	private BigInteger z;
+
+	private BigInteger G;
+
+	private BigInteger Gx;
+	
+	private BigInteger P;
+
+	public Cifrado(BigInteger gmain, BigInteger pmain, BigInteger gxmain) {
+		this.G = gmain;
+		this.P = pmain;
+		this.Gx = gxmain;
+	}
+
+		
+
 
 }
